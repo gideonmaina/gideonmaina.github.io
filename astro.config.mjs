e6330ed4c0b1b9235aee7d9c6a14d40175671ee3
ECHOsOdyssey <gideon-yebei@proton.me>
2025-07-09 18:46:47 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://gideonmaina.me',
   output: 'server',
